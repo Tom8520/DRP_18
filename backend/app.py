54e0bfa07f93885615aa5489aa1dc7bf646894ee
tom8520 <nathan.grange@outlook.com>
2024-05-27 16:01:17 +0100

    add start/stop scripts for aws

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 CORS(app, support_credentials=True)
 
 
