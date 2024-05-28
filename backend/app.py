f90405ef69313870573a062fad2f1031259d1ff5
tom8520 <nathan.grange@outlook.com>
2024-05-28 09:51:22 +0100

    make nginx serve static files instead of flask

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_url_path='', static_folder='static')
+app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 
