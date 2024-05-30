ce5215a9d418abb8b4fc47a2b748cb4252b4ae01
tom8520 <nathan.grange@outlook.com>
2024-05-30 11:12:21 +0100

    make the upload file button functional

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import React, {ChangeEventHandler,useState} from 'react';
+import { Link } from 'react-router-dom';
 import './../styles/Landing.css';
 
 const UploadImagePage = () => {
 
-  const handleUploadImage = () => {
-    
+  const [image, setImage] = useState("");
+
+  const handleUploadImage: ChangeEventHandler<HTMLInputElement> = (e) => {
+    try {
+      const file = e.target.files?.[0];
+
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onerror = (err) => {
+          console.log(err);
+        };
+        reader.onload = () => {
+          setImage(reader.result as string);
+        };
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-    return (
+  return (
       <div>
         <div className="header">
           <h1>Camera</h1>
@@ -19,10 +36,12 @@ const UploadImagePage = () => {
             <Link to="/login" className="styled-button">Login</Link>
           </div>  
           <div className="button-container">
-            <button className="styled-button" onClick={handleUploadImage}>Upload Image</button>
+            <input id="file-upload" onChange={e => handleUploadImage(e)} type="file" accept="image/*" hidden/>
+            <label htmlFor="file-upload" className="styled-button">Upload Image</label>
           </div>
         </div>
         <div className="imageContainer">
+          <img alt="Upload an image" src={image} className="imagePreview"/>
         </div>
       </div>
     );
