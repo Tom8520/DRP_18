99856a2b637d274e2ad6c51e74c88c7f4dc02120
tom8520 <nathan.grange@outlook.com>
2024-06-18 16:24:15 +0100

    fix nginx

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,16 +4,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {useNavigate} from "react-router-dom";
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
 
   return (
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="">AI Diagram Tool</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/main">Upload</Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
+            <Nav.Link onClick={() => navigate("/main")}>Upload</Nav.Link>
             <Nav.Link href={"/user"}>Account</Nav.Link>
             <Nav.Link href="/login">Log In</Nav.Link>
           </Nav>
