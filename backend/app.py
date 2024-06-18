7d35a49fe016522e96d7afbf7d50cecc407c2aa2
tom8520 <nathan.grange@outlook.com>
2024-06-18 12:21:21 +0100

    added comments

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 import base64
 import os
+
+import botocore
 from flask import request, jsonify, send_file
 from flask_cors import CORS, cross_origin
 
@@ -12,7 +14,6 @@ import boto3
 
 load_dotenv()
 
-s3 = boto3.client('s3', region_name='eu-north-1')
 # with open('tmp/image.png', 'wb') as f:
 #     s3.download_fileobj('drp18-image-storage', 'Screenshot from 2024-06-05 14-32-00.png', f)
 
@@ -53,6 +54,7 @@ def upload_image(user):
 
     s3_filename = generate_jwt_token(user) + ".png"
 
+    s3 = boto3.client('s3', region_name='eu-north-1')
     s3.upload_file(f"tmp/{user}.{file.filename.split('.')[-1]}", os.getenv("S3_BUCKET_NAME"), s3_filename)
     upload_image_controller(user, s3_filename, True if shared == "true" else False)
 
@@ -81,6 +83,7 @@ def download_image(user):
     if not verify_image_owner(user, filename):
         return jsonify({"message": "unauthorized"}), 401
 
+    s3 = boto3.client('s3', region_name='eu-north-1')
     with open(f'tmp/{user}.png', 'wb') as f:
         s3.download_fileobj(os.getenv("S3_BUCKET_NAME"), filename, f)
 
@@ -88,6 +91,63 @@ def download_image(user):
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
     return jsonify({'image': encoded_string})
+
+
+@app.route("/api/getComments", methods=['GET'])
+@cross_origin(supports_credentials=True)
+@verify_jwt_token
+def get_comments(user):
+    filename = request.args.get("filename")
+
+    if filename is None:
+        return jsonify({"message": "no filename"}), 400
+
+    if not verify_image_owner(user, filename):
+        return jsonify({"message": "unauthorized"}), 401
+
+    s3 = boto3.client('s3', region_name='eu-north-1')
+    with open(f'tmp/{user}.txt', 'wb') as f:
+        s3.download_fileobj(os.getenv("S3_BUCKET_NAME"), filename + "_notes.txt", f)
+
+    with open(f'tmp/{user}.txt', 'r') as file:
+        notes = file.read()
+
+    return jsonify({"comments": notes.split("\n")})
+
+
+@app.route("/api/saveComment", methods=['POST'])
+@cross_origin(supports_credentials=True)
+@verify_jwt_token
+def save_comment(user):
+    filename = request.form.get("filename")
+    comment = request.form.get("comment")
+
+    if filename is None:
+        return jsonify({"message": "no filename"}), 400
+
+    if not verify_image_owner(user, filename):
+        return jsonify({"message": "unauthorized"}), 401
+
+    s3 = boto3.client('s3', region_name='eu-north-1')
+    try:
+        with open(f'tmp/{user}.txt', 'wb') as f:
+            s3.download_fileobj(os.getenv("S3_BUCKET_NAME"), filename + "_notes.txt", f)
+
+        with open(f'tmp/{user}.txt', 'r') as file:
+            notes = file.read()
+    except botocore.exceptions.ClientError as e:
+        notes = ""
+
+    notes += comment + "\n"
+
+    with open(f'tmp/{user}.txt', 'w') as file:
+        file.write(notes)
+
+    s3.upload_file(f"tmp/{user}.txt", os.getenv("S3_BUCKET_NAME"), filename + "_notes.txt")
+
+    return jsonify({"comments": notes.split("\n")})
+
+
 
 
 @app.route("/api/secret", methods=['GET', 'POST'])
